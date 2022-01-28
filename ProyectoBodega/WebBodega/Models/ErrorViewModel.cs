b1c8a3f7bdab76c3281d15a62b2ba3640c023e1a
5talin 5 <59964372+5talinFive@users.noreply.github.com>
2022-01-28 17:25:20 -0500

    Add files via upload

---
+++ ProyectoBodega/WebBodega/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebBodega.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
